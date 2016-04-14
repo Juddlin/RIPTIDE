1cacf1bbde31ef52eac590921f6e119889b7b408
Garrett Goodman <goodman.27@hotmail.com>
2016-04-14 18:10:50 -0400

    Bluetooth Implementation
    App now reads the heart rate from a connected Polar H7 heart rate
    monitor. It also sends an email correctly formatted per team mates
    request.

--- app/src/main/java/wsuteams/riptide/RunningActivity.java
+++ app/src/main/java/wsuteams/riptide/RunningActivity.java
@@ -8,6 +8,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Handler;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
@@ -16,14 +17,44 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import java.util.Calendar;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import java.util.List;
+import java.util.UUID;
 
 public class RunningActivity extends AppCompatActivity {
+    // GPS related variables
     private Location gpsLocation = null;
     private Handler handler;
     private LocationManager locationManager;
     private static final int GPS_TIME_INTERVAL = 60000; // Get GPS location every 1 min
     private static final int GPS_DISTANCE = 5; // Set the distance value in meter
+
+    // Bluetooth related variables
+    private static final UUID HEART_RATE_UUID = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb");
+    private static final UUID HEART_RATE_SERVICE_UUID = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb");
+    private static final String deviceAddress = "00:22:D0:9E:9E:57";
+    private static final long SCAN_PERIOD = 10000;
+    private static BluetoothGattDescriptor descriptor;
+    private static BluetoothGattCharacteristic cc;
+    private Button scanButton;
+    private TextView heartRateStatusView;
+    private Handler mHandler = new Handler();
+    private boolean scanning;
+    private boolean connected = false;
+    private BluetoothDevice finalDevice;
+    private BluetoothGatt mBluetoothGatt;
+    private BluetoothGattCharacteristic heartRateCharacteristic;
+    private int theHeartRate = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +62,9 @@ public class RunningActivity extends AppCompatActivity {
         setContentView(R.layout.activity_running);
         final SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
         final SharedPreferences.Editor editor = pref.edit();
+
+        // Initialize text view for showing connected device
+        heartRateStatusView = (TextView) findViewById(R.id.heartRateMonitorStatusTextView);
 
         // Fill the server field on create
         final EditText serverTextField = (EditText) findViewById(R.id.serverTextField);
@@ -50,6 +84,15 @@ public class RunningActivity extends AppCompatActivity {
                 ComponentName cn = intent.getComponent();
                 Intent mainIntent = IntentCompat.makeRestartActivityTask(cn);
                 startActivity(mainIntent);
+            }
+        });
+
+        // Button for starting the scan for BLE devices
+        scanButton = (Button) findViewById(R.id.heartRateScanButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scan(true);
             }
         });
 
@@ -104,7 +147,8 @@ public class RunningActivity extends AppCompatActivity {
                             SmsManager sms = SmsManager.getDefault();
                             double latitude = gpsLocation.getLatitude();
                             double longitude = gpsLocation.getLongitude();
-                            sms.sendTextMessage("6245", null, email.getText().toString() + " Latitude: " + formatLatLong(latitude) + " Longitude: " + formatLatLong(longitude), null, null);
+
+                            sms.sendTextMessage("6245", null, email.getText().toString() + " " + formatHeart(theHeartRate) + formatLatLong(latitude) + formatLatLong(longitude), null, null);
                             handler.postDelayed(this, HANDLER_DELAY);
                         }
                     }, HANDLER_DELAY);
@@ -122,6 +166,125 @@ public class RunningActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    /**
+     * Method triggers once the current app intent closes. It disconnects the Gatt profile and the device.
+     */
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(mBluetoothGatt != null){
+            mBluetoothGatt.disconnect();
+            mBluetoothGatt = null;
+            connected = false;
+            finalDevice = null;
+        }
+    }
+
+    /**
+     * Method scans for Bluetooth Low Energy Devices.
+     */
+    private void scan(final boolean enable){
+        final BluetoothAdapter adapter = getmBluetoothAdapter();
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    scanning = false;
+                    adapter.stopLeScan(callback);
+                }
+            }, SCAN_PERIOD);
+
+            scanning = true;
+            adapter.startLeScan(callback);
+        } else {
+            scanning = false;
+            adapter.stopLeScan(callback);
+        }
+    }
+
+    /**
+     * Variable callback is used for the Override method "onLeScan" which will get the result of the scan
+     * and connect the device if it is the correct one that RIPTIDE is looking for.
+     */
+    private final BluetoothAdapter.LeScanCallback callback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(device.getAddress().equals("00:22:D0:9E:9E:57")) {
+                        // list.add(device);
+                        finalDevice = device;
+                        connectDevice();
+                    }
+                }
+            });
+        }
+    };
+
+    /**
+     * Helper method to connect the Bluetooth device to the RIPTIDE app.
+     */
+    private void connectDevice(){
+        if(connected == false) {
+            connected = true;
+            mBluetoothGatt = finalDevice.connectGatt(this, false, mGattCallback);
+            mBluetoothGatt.connect();
+            heartRateStatusView.setText("Device: " + finalDevice.getName());
+        }
+    }
+
+    /**
+     * mGattCallBack variable is used for the Override methds onConnectionStateChange, onServicesDiscovered, and onCharacteristicChanged.
+     * onConnectionStateChange - is called when the device detects a state change triggered from the app. The initial call will trigger
+     *                           the discovery of the services available on the device.
+     * onServicesDiscovered - is called from the device directly after the services have been discovered from onConnectionStateChange.
+     *                        This method will write a characteristic to the device telling it to start transmitting the heart rate.
+     * onCharacteristicChanged - is called whenever the device transmits the heart rate. It will update a global member variable with
+     *                          the current heart rate.
+     */
+    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState){
+            String intentAction;
+            if(newState == BluetoothProfile.STATE_CONNECTED){
+                gatt.discoverServices();
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status){
+            BluetoothGattService service = gatt.getService(HEART_RATE_SERVICE_UUID);
+            List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
+            for (BluetoothGattCharacteristic cc : characteristics){
+                for (BluetoothGattDescriptor descriptor : cc.getDescriptors()){
+                    RunningActivity.descriptor = descriptor;
+                    RunningActivity.cc = cc;
+                    gatt.setCharacteristicNotification(cc, true);
+                    descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    gatt.writeDescriptor(descriptor);
+                }
+            }
+
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic){
+            byte[] data = characteristic.getValue();
+            int bmp = data[1] & 0xFF; // Unsign the return value
+            theHeartRate = bmp;
+        }
+    };
+
+    /**
+     * Method returns the Bluetooth adapter from the BluetoothManager object
+     */
+    private BluetoothAdapter getmBluetoothAdapter() {
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        return bluetoothManager.getAdapter();
     }
 
     /**
